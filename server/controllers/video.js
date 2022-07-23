8e1903764cdb1bd42a73f7b1bec564783a97e396
Ramit <ramitsarkar902@gmail.com>
2022-07-23 17:26:09 +0530

    almost done

---
+++ server/controllers/video.js
@@ -0,0 +1,114 @@
+import User from "../models/User.js";
+import Video from "../models/Video.js";
+import { createError } from "../error.js";
+
+export const addVideo = async (req, res, next) => {
+  const newVideo = new Video({ ...req.body, userId: req.user.id });
+  try {
+    let savedVideo = await newVideo.save();
+    res.status(200).json(savedVideo);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, "Video not found"));
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+      );
+      res.status(200).json(updatedVideo);
+    } else {
+      return next(
+        createError(403, "You are not authorized to update this video")
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteVideo = async (req, res, next) => {
+  try {
+    const video = Video.findById(req.params.id);
+    if (!video) return next(createError(404, "Video not found"));
+    if (req.user.id === video.userId) {
+      await Video.findByIdAndDelete(req.params.id);
+      res.status(200).json("Video deleted successfully");
+    } else {
+      return next(
+        createError(403, "You are not authorized to delete this video")
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, "Video not found"));
+    res.status(200).json(video);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const addView = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(createError(404, "Video not found"));
+    video.views += 1;
+    await video.save();
+    res.status(200).json(video);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const random = async (req, res, next) => {
+  try {
+    const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+    res.status(200).json(videos);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const trend = async (req, res, next) => {
+  try {
+    const videos = await Video.find({}).sort({ views: -1 });
+    res.status(200).json(videos);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const sub = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+    const subscribedChannels = user.subscribedUsers;
+    let list = [];
+    await Promise.all(
+      subscribedChannels.map(async (channelId) => {
+        list.push(await Video.find({ userId: channelId }));
+      })
+    );
+
+    res
+      .status(200)
+      .json(
+        list
+          .flat()
+          .sort((a, b) => b.createdAt - a.createdAt || b.views - a.views)
+      );
+  } catch (err) {
+    next(err);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
@@ -16,6 +21,25 @@ const connect = () => {
       throw err;
     });
 };
+
+app.use(express.json());
+app.use(cookieParser());
+
+//custom error handling
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  let message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message: message,
+  });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server started");
